94da4b50d0ecccb4b1c5298252b41f9fb7f22586
Daniel Quintero <dgquintero02@hotmail.com>
2021-09-18 09:24:23 -0500

    sanitizar info form

--- UpTaskNodeJs/controllers/proyectosController.js
+++ UpTaskNodeJs/controllers/proyectosController.js
@@ -1,4 +1,7 @@
 const Proyectos = require('../models/Proyectos');
+const slug = require('slug');
+
+
 
 exports.proyectosHome =  (req, res) => {
   res.render('index', {
@@ -34,7 +37,8 @@ exports.nuevoProyecto = async (req, res) => {
   } else {
     // No hay errores
     // Insertar en la BD
-    const proyecto = await Proyectos.create({ nombre });
+    const url = slug(nombre).toLowerCase();
+    const proyecto = await Proyectos.create({ nombre, url });
     res.redirect('/');
   }
 }
