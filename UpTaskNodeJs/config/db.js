891052ee9e0ade95b7d3f568cfaaa5e336118446
Daniel Quintero <dgquintero02@hotmail.com>
2022-02-15 15:22:06 -0500

    confirmación email creacion de cuenta y configurando variables de entorno

--- UpTaskNodeJs/config/db.js
+++ UpTaskNodeJs/config/db.js
@@ -1,22 +1,30 @@
 const { Sequelize } = require('sequelize');
- 
+
+// estraer valores de variables
+require('dotenv').config({ path: 'variables.env'})
+
+
 // Option 1: Passing parameters separately
-const db = new Sequelize('uptasknode', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3306',
-    operatorsAliases: false,
-    define: {
-        // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-        // This was true by default, but now is false by default
-        timestamps: false
-    },
-    pool:{
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-});
+const db = new Sequelize(
+    process.env.BD_NOMBRE,
+    process.env.BD_USER,
+    process.env.BD_PASS,
+    {
+      host: process.env.BD_HOST,
+      dialect: 'mysql',
+      port: process.env.BD_PORT,
+      operatorsAliases: false,
+      define: {
+          // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
+          // This was true by default, but now is false by default
+          timestamps: false
+      },
+      pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    });
 
 module.exports = db;
--- UpTaskNodeJs/controllers/usuariosController.js
+++ UpTaskNodeJs/controllers/usuariosController.js
@@ -1,5 +1,5 @@
 const Usuarios = require('../models/Usuarios')
-
+const enviarEmail = require('../handlers/email')
 
 exports.formCrearCuenta = (req, res ) => {
   res.render('crearCuenta', {
@@ -24,6 +24,25 @@ exports.crearCuenta = async (req, res) =>{
       email,
       password 
     })
+    // crear una URL de confirmar
+    const confirmarUrl = `http://${req.headers.host}/confirmar/${email}`
+    // crear el objeto de usuario
+
+    const usuario = {
+      email
+    }
+    // enviar email
+    await enviarEmail.enviar({
+      usuario,
+      subject: 'Confirma tu cuenta UpTask',
+      confirmarUrl,
+      archivo: 'confirmar-cuenta'
+    })
+    // redirigir al usuario
+    req.flash('correcto', 'Enviamos un correo, confirma tu cuenta')
+
+
+
     res.redirect('iniciar-sesion')
   } catch (error) {
     req.flash('error', error.errors.map(error => error.message))
@@ -41,4 +60,27 @@ exports.formReestablecerPassword = (req, res) => {
   res.render('reestablecer', {
     nombrePagina: 'Reestablecer tu Contraseña'
   })
+}
+
+
+// Cambia el estado de una cuenta
+exports.confirmarCuenta = async (req, res)=>{
+  const usuario = await Usuarios.findOne({
+    where:{
+      email: req.params.correo
+    }
+  })
+
+  // si no existe el usuario
+  if(!usuario){
+    req.flash('error', 'No valido')
+    res.redirect('/crear-cuenta')
+  }
+
+  usuario.activo = 1
+  await usuario.save()
+
+
+  req.flash('correcto', 'Cuenta activada correctamente')
+  res.redirect('/iniciar-sesion')
 }
